34bf7bd75d31c47fb6362df30aa53b8d6d48084e
Matheus <matheusalmeida016@gmail.com>
2021-10-09 01:22:41 -0700

    Clear app and add normalize.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import 'styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'normalize.css'
 
 function MyApp ({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
 
 export default MyApp

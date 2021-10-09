f12dcc7c66584f1a7874653193f2ce40c3907b5c
Matheus <matheusalmeida016@gmail.com>
2021-10-09 02:19:57 -0700

    Add styled-components

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const withPWA = require('next-pwa')
 
 module.exports = withPWA({
+  webpack5: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false
+    }
+    return config
+  },
   reactStrictMode: true,
   pwa: {
     dest: 'public',
